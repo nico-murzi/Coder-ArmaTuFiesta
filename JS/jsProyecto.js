9748e04bd1f2254df75aa6dfdcec232a837018e1
nico-murzi <nicolasmatiasmurzi@gmail.com>
2021-10-21 19:59:51 -0300

    Modificaciones con Jquery

--- JS/jsProyecto.js
+++ JS/jsProyecto.js
@@ -1,5 +1,7 @@
 // Se define un constructor de usuarios y la lista de usuarios mediante array
 
+
+
 class Usuario {
   constructor(user, email, wattsTotales, precioAudio) {
     this.user = user;
@@ -9,12 +11,6 @@ class Usuario {
   }
 }
 
-// const Users = [
-//   { usuario: "jose", contraseña: "jose123" },
-//   { usuario: "juan", contraseña: "juan123" },
-//   { usuario: "julio", contraseña: "agosto123" },
-//   { usuario: "diego", contraseña: "diego123" },
-// ];
 
 //  Validacion de identidad para entrada al sitio
 
@@ -30,95 +26,32 @@ let lugarAbierto;
 let lugarCerrado;
 let datosCompletos = true;
 
-// const validarIdentidad = () => {
-//   let bandera = false;
-//   let i = 0;
-//   let intento = 1;
-//   let secreto;
-//   let identidad;
-
-//   while (intento <= 3) {
-//     i = 0;
-//     identidad = prompt(`Intento N°${intento}. Ingrese su usuario:`);
-//     secreto = prompt(`Intento N°${intento}. Ingrese su contraseña:`);
-//     while (!bandera && i < Users.length) {
-//       if (Users[i].usuario === identidad && Users[i].contraseña === secreto) {
-//         bandera = true;
-//         intento = 5;
-//         alert(`Contraseña correcta!`);
-//       } else {
-//         i++;
-//       }
-//     }
-//     intento++;
-//   }
-
-//   if (intento === 4) {
-//     alert(`Ingreso el maximo de intentos incorrectos`);
-//     bandera1 = false;
-//   }
-// };
-
-// validarIdentidad();
-
-// Ingreso de datos
-
-
-// Se crea y se agrega el nuevo usuario al array de usuarios
-
-
-
-// console.log(newUser);
-
-// Users.push(newUser);
-
-// Se le informa al usuario los resultados
-
-// console.log(
-//   `${newUser.user}, el precio total del audio es de ${precioAudio}`
-// );
-// alert(`${newUser.user}, el precio total del audio es de ${precioAudio}`);
-// console.log(`Le enviaremos el presupuesto a su correo ${newUser.email}`);
-// alert(`Le enviaremos el presupuesto a su correo ${newUser.email}`);
-
-//   console.log(Users);
-// } else {
-//   // Si el usuario ingresa mal los datos mas de 3 veces, se deberá reiniciar la pestaña
-//   alert("Reinicie la pestaña para continuar");
-// }
 
 // Se agregan los resultados al HTML por medio de DOM
 
 
-let boton = document.querySelector(".ATFButton")
-boton.addEventListener("click", ATFRespuesta)
-function ATFRespuesta() {
+$('.ATFButton').on('click', () => {
 
-  nombre = document.querySelector("#nombre").value;
-  email = document.querySelector("#email").value;
-  apellido = document.querySelector("#apellido").value;
-  cantidadInvitados = document.querySelector("#cantidad_invitados").value;
+
+  nombre = $('#nombre').val();
+  email = $("#email").val();
+  apellido = $("#apellido").val();
+  cantidadInvitados = $("#cantidad_invitados").val();
   lugarAbierto = document.getElementById("Aire libre").checked;
   lugarCerrado = document.getElementById("Cerrado").checked;
   datosCompletos = true;
-
+  
   if (datosCompletos == true) {
-    // user = prompt(`Ingrese su nombre de usuario nuevamente:`);
-    // email = prompt(`Ingrese su email:`);
-
-    // cantidadInvitados = parseInt(prompt(`Ingrese la cantidad de invitados`));
-    // let lugar = prompt(
-    //   `Ingrese "abierto" o "cerrado" dependiendo del tipo de lugar`
-    // );
+   
     wattsNecesarios = 0;
     precioAudio = 0;
-
+  
     // Calculador de datos
-
+  
     
     const wattsTotales = () => {
-
-
+  
+  
       if (lugarCerrado) {
         switch (true) {
           case cantidadInvitados <= 50:
@@ -171,56 +104,75 @@ function ATFRespuesta() {
         console.log(
           `Se necesitan ${wattsNecesarios} Watts de potencia para tu fiesta`
         );
+        
       } else {
         alert("Seleccione el tipo de lugar");
-        wattsTotales();
+        location.reload();
       }
     };
-
+  
     // Calculador de precio en funcion de los Watts necesarios
-
+  
     let precioWatts = 45;
-
+  
     const precioTotal = () => {
       precioAudio += wattsNecesarios * precioWatts;
     };
-
+  
     // Declaracion
-
+  
     wattsTotales();
     precioTotal();
-
+  
   } else {
     alert("Complete todos los campos obligatorios")
+    
   }
-
+  
   let newUser = new Usuario(`${nombre} ${apellido}`, email, wattsNecesarios, precioAudio)
   console.log(newUser);
   localStorage.setItem("usuario", JSON.stringify(newUser));
   
-
-
+  
+  
   if (datosCompletos == true) {
 
-    const respuesta = document.querySelector(".atf_contenedor_general");
-
-    respuesta.innerHTML += `<div class="atf_contenedor_general">
-            <div class="atf_contenedor">
-            <h2 class="consulta_titulo">bienvenido ${nombre} ${apellido} <span>tu presupuesto esta listo</span></h2>
-            <h3 class="respuestaJS">Su fiesta es para <span>${cantidadInvitados}</span> invitados<h3>
-            <h3 class="respuestaJS">El precio total del audio es de <span>$${precioAudio}</span></h3>
-            <h3 class="respuestaJS">Le enviaremos el presupuesto a su correo <span>${email}</span></h3>
-            </div>
-            </div>`;
-
+    $('.atf_contenedor_general').append(`<div class="atf_contenedor_general">
+    <div class="atf_contenedor">
+    <div class="formulario_contacto atf_contacto">
+    <h2 class="consulta_titulo tituloJS">bienvenido ${nombre} ${apellido} <span>tu presupuesto esta listo</span></h2>
+    <h3 class="respuestaJS">Su fiesta es para <span>${cantidadInvitados}</span> invitados<h3>
+    <h3 class="respuestaJS">El precio total del audio es de <span>$${precioAudio}</span></h3>
+    <h3 class="respuestaJS">Le enviaremos el presupuesto a su correo <span>${email}</span></h3>
+    </div>
+    </div>
+    </div>`);
+  
+    // const respuesta = document.querySelector(".atf_contenedor_general");
+  
+    // respuesta.innerHTML += `<div class="atf_contenedor_general">
+    //         <div class="atf_contenedor">
+    //         <div class="formulario_contacto atf_contacto">
+    //         <h2 class="consulta_titulo tituloJS">bienvenido ${nombre} ${apellido} <span>tu presupuesto esta listo</span></h2>
+    //         <h3 class="respuestaJS">Su fiesta es para <span>${cantidadInvitados}</span> invitados<h3>
+    //         <h3 class="respuestaJS">El precio total del audio es de <span>$${precioAudio}</span></h3>
+    //         <h3 class="respuestaJS">Le enviaremos el presupuesto a su correo <span>${email}</span></h3>
+    //         </div>
+    //         </div>
+    //         </div>`;
+  
   } else {
-
+  
     const respuesta = document.querySelector(".atf_contenedor_general");
-
+  
     respuesta.innerHTML += `<div class="atf_contenedor_general">
             <div class="atf_contenedor">
               <h2 class="consulta_titulo">Superaste el limite de intentos <span>refresca la pestaña</span></h2>
               </div>
               </div>`;
   }
-}
+
+  $('.principal').slideUp(1200);
+});
+
+
